9fd99449b27205ad24fa3414413158d2fb0f26f7
Osvaldo Souza Ferreira <osvald.soza@gmail.com>
2023-07-18 21:20:12 -0300

    inclue scss

--- src/components/botao/botao.tsx
+++ src/components/botao/botao.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./style.scss";
 
 class Botao extends React.Component {
   render() {
-    return <button>Clique</button>;
+    return <button className="botao">Clique</button>;
   }
 }
 
